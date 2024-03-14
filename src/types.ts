e1f7066eaefeabb2c5717a2c3a3301ee9471ff98
kavindamkumarage <kavindakumarage@gmail.com>
2024-03-14 12:03:58 +0530

    initial commit
    updated data set added
    function added for getCountryByCode

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type CountryData = {
+    country: string;
+    phoneNumberLengthByCountry_phLengthMax: number |null;
+    phoneNumberLengthByCountry_phLengthMin: number | null;
+    phoneNumberLengthByCountry_phLength: string | number;
+    phoneNumberLengthByCountry_CountryCode: number | null;
+    phoneNumberLengthByCountry_phLength_InternationalPrefix: number | null;
+    phoneNumberLengthByCountry_phLength_NationalPrefix: number | null;
+    phoneNumberLengthByCountry_Notes: string | null;
+}
